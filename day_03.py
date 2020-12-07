c7ce6fb651efd4f28e3206a1c1e6dccad4e2059a
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2020-12-07 19:33:00 +0100

    Refactor day 03

--- day_03.py
+++ day_03.py
@@ -1,26 +1,27 @@
 import fileinput
+from collections.abc import Iterable
 from functools import partial, reduce
 from itertools import count, islice
 from operator import mul
 
 
 def main():
-    grid = list(map(str.strip, fileinput.input()))
+    grid = list(map(str.rstrip, fileinput.input()))
 
     print(part_1(grid))
     print(part_2(grid))
 
 
-def part_1(grid: list[str]) -> int:
+def part_1(grid: Iterable[str]) -> int:
     return trees_on_the_slope(grid, 3, 1)
 
 
-def part_2(grid: list[str]) -> int:
+def part_2(grid: Iterable[str]) -> int:
     # noinspection PyTypeChecker
     return reduce(mul, map(partial(trees_on_the_slope, grid), (1, 3, 5, 7, 1), (1, 1, 1, 1, 2)))
 
 
-def trees_on_the_slope(grid: list[str], right: int, down: int) -> int:
+def trees_on_the_slope(grid: Iterable[str], right: int, down: int) -> int:
     return sum(1 for row, col_idx in zip(islice(grid, 0, None, down), count(0, right))
                if row[col_idx % len(row)] == '#')
 
