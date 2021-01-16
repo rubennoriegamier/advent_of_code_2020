8d0f4149468097a8789874ac7e0e365a651b469b
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2021-01-16 15:49:10 +0100

    Day 23, part 2 with NumPy & Numba

--- day_23.py
+++ day_23.py
@@ -1,29 +1,30 @@
-from itertools import chain
+import numba as nb
+import numpy as np
 
 
 def main():
-    cups = list(map(int, input()))
+    cups = np.fromiter(map(int, input()), dtype=np.int64)
 
     print(part_1(cups, 100))
-    print(part_2(cups, 10_000_000))
+    print(part_2(cups, 1_000_000, 10_000_000))
 
 
-def game(cups, moves):
-    right = []
+def part_1(cups, moves):
+    next_cup_idx = list(range(1, len(cups) + 1))
+    next_cup_idx[-1] = 0
+
     indexes = [0] * (len(cups) + 1)
-
     for idx, cup in enumerate(cups):
-        right.append((idx + 1) % len(cups))
         indexes[cup] = idx
 
     idx = 0
 
     for _ in range(moves):
-        pick_up_a_idx = right[idx]
-        pick_up_b_idx = right[pick_up_a_idx]
-        pick_up_c_idx = right[pick_up_b_idx]
+        pick_up_a_idx = next_cup_idx[idx]
+        pick_up_b_idx = next_cup_idx[pick_up_a_idx]
+        pick_up_c_idx = next_cup_idx[pick_up_b_idx]
 
-        right[idx] = right[pick_up_c_idx]
+        next_cup_idx[idx] = next_cup_idx[pick_up_c_idx]
 
         destination = cups[idx] - 1
 
@@ -42,27 +43,69 @@ def game(cups, moves):
 
         destination_idx = indexes[destination]
 
-        right[pick_up_c_idx] = right[destination_idx]
-        right[destination_idx] = pick_up_a_idx
+        next_cup_idx[pick_up_c_idx] = next_cup_idx[destination_idx]
+        next_cup_idx[destination_idx] = pick_up_a_idx
 
-        idx = right[idx]
+        idx = next_cup_idx[idx]
 
     idx = indexes[1]
+    result = ''
 
     for _ in range(len(cups) - 1):
-        idx = right[idx]
+        idx = next_cup_idx[idx]
 
-        yield cups[idx]
+        result += str(cups[idx])
 
-
-def part_1(cups, moves):
-    return ''.join(map(str, game(cups, moves)))
+    return result
 
 
-def part_2(cups, moves):
-    g = game(list(chain(cups, range(max(cups) + 1, 1_000_001))), moves)
+@nb.njit(nb.int64(nb.int64[:], nb.int64, nb.int64))
+def part_2(initial_cups, size, moves):
+    cups = np.arange(1, size + 1, dtype=np.int64)
+    cups[0:len(initial_cups)] = initial_cups
 
-    return next(g) * next(g)
+    next_cup_idx = np.arange(1, size + 1, dtype=np.int64)
+    next_cup_idx[-1] = 0
+
+    indexes = np.arange(-1, size, dtype=np.int64)
+    for idx, cup in enumerate(initial_cups):
+        indexes[cup] = idx
+
+    idx = 0
+
+    for _ in range(moves):
+        pick_up_a_idx = next_cup_idx[idx]
+        pick_up_b_idx = next_cup_idx[pick_up_a_idx]
+        pick_up_c_idx = next_cup_idx[pick_up_b_idx]
+
+        next_cup_idx[idx] = next_cup_idx[pick_up_c_idx]
+
+        destination = cups[idx] - 1
+
+        while (destination == cups[pick_up_a_idx] or
+               destination == cups[pick_up_b_idx] or
+               destination == cups[pick_up_c_idx]):
+            destination -= 1
+
+        if destination == 0:
+            destination = len(cups)
+
+            while (destination == cups[pick_up_a_idx] or
+                   destination == cups[pick_up_b_idx] or
+                   destination == cups[pick_up_c_idx]):
+                destination -= 1
+
+        destination_idx = indexes[destination]
+
+        next_cup_idx[pick_up_c_idx] = next_cup_idx[destination_idx]
+        next_cup_idx[destination_idx] = pick_up_a_idx
+
+        idx = next_cup_idx[idx]
+
+    idx_a = next_cup_idx[indexes[1]]
+    idx_b = next_cup_idx[idx_a]
+
+    return cups[idx_a] * cups[idx_b]
 
 
 if __name__ == '__main__':
--- day_23_test.py
+++ day_23_test.py
@@ -1,17 +1,19 @@
 import unittest
 
+import numpy as np
+
 from day_23 import part_1, part_2
 
 
 class TestDay23(unittest.TestCase):
     def setUp(self):
-        self._cups = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+        self._cups = np.array([3, 8, 9, 1, 2, 5, 4, 6, 7], dtype=np.int64)
 
     def test_part_1(self):
         self.assertEqual(part_1(self._cups, 100), '67384529')
 
     def test_part_2(self):
-        self.assertEqual(part_2(self._cups, 10_000_000), 149_245_887_792)
+        self.assertEqual(part_2(self._cups, 1_000_000, 10_000_000), 149_245_887_792)
 
 
 if __name__ == '__main__':
